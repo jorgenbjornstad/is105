d205ab20b1922e3680272145d6f999ecf0b4a452
vebnor <vebjon16@uia.no>
2017-01-26 18:51:09 +0100

    Golang-fil som alle bør gjøre en endring i

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+"fmt"
+"math")
+
+var y, c float64 = 8, 20
+func main() {
+x := math.Log2(y)
+z := math.Log10(c)
+    fmt.Printf("hello, world\n")
+    fmt.Println(x, z)
+}
