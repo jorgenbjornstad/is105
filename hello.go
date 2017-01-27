31c99016752419e48608caa6476e767ac68c4699
Snorre <snorre_btunge@hotmail.com>
2017-01-27 13:51:51 +0100

    Update hello.go

--- hello.go
+++ hello.go
@@ -10,4 +10,5 @@ x := math.Log2(y)
 z := math.Log10(c)
     fmt.Printf("Hello, Vebjørn\n")
     fmt.Println(x, z)
+    fmt.Println("Jeg bare skriver det samme som dere skrev, men har ikke mye peiling på hva det betyr")
 }
