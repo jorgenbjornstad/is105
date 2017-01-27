ad8b1ef9b910b86079fccae3a14f58ad5a96a60a
vebnor <vebjon16@uia.no>
2017-01-27 15:09:45 +0100

    oppgave 4

---
+++ log/funksjoner/log.go
@@ -0,0 +1,9 @@
+package funksjoner
+
+import "math"
+
+func Log2() float64 {
+	var x float64 = 10
+	var result = math.Log2(x)
+	return result
+}
---
+++ log/main.go
@@ -0,0 +1,10 @@
+package main
+
+import ("fmt"
+	"LogProsjekt/funksjoner"
+)
+
+func main() {
+	fmt.Println(funksjoner.Log2())
+
+}
