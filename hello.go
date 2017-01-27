b05f2f4be2210c6ee9f2314eab1bdfd9a7088fc5
vebnor <vebjon16@uia.no>
2017-01-27 14:58:13 +0100

    update hello.go

--- hello.go
+++ hello.go
@@ -4,11 +4,13 @@ import (
 "fmt"
 "math")
 
-var y, c float64 = 8, 20
+
 func main() {
+    var y float64 = 8
 x := math.Log2(y)
-z := math.Log10(c)
     fmt.Printf("Hello, Vebjørn\n")
-    fmt.Println(x, z)
+    fmt.Println(x)
     fmt.Println("Jeg bare skriver det samme som dere skrev, men har ikke mye peiling på hva det betyr")
+    fmt.Println("Lønner seg å sette seg ned med tutorialen :)\nEr ganske likt java\nFunksjonene er bare fra standardbiblioteket")
+
 }
