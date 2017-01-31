c463cc3fec5654e18814f5db9b0cb4f739ae0397
juditheikaas <juditheikaas@judiths-air.uia.no>
2017-01-31 11:18:33 +0100

    j igjen

--- hello.go
+++ hello.go
@@ -8,19 +8,12 @@ import (
  func main() {
      var y float64 = 8
  x := math.Log2(y)
-    fmt.Printf("Hello, Vebjørn\n")
+    fmt.Printf("Hello, VebjÃ¸rn\n")
     fmt.Println(x)
-<<<<<<< HEAD
     fmt.Println("Jeg bare skriver det samme som dere skrev, men har ikke mye peiling pÃ¥ hva det betyr")
-    fmt.Println("JJ HEIÂ»)
+    fmt.Println("JJ is backÂ»)
     fmt.Println("LÃ¸nner seg Ã¥ sette seg ned med tutorialen :)\nEr ganske likt java\nFunksjonene er bare fra standardbiblioteket")
   fmt.Println("HÃ¥per du kan lÃ¦re meg litt av dette Vebbis!")
-=======
-    fmt.Println("Jeg bare skriver det samme som dere skrev, men har ikke mye peiling på hva det betyr")
-    fmt.Println("JJ")
-    fmt.Println("Lønner seg å sette seg ned med tutorialen :)\nEr ganske likt java\nFunksjonene er bare fra standardbiblioteket")
-  fmt.Println("Håper du kan lære meg litt av dette Vebbis!")
->>>>>>> a0bbd6d5db81d8ca16e5103a7c68b974fc63176d
 <<<<<<< HEAD
     fmt.Println("Men da fikk vi det til da")
 =======
