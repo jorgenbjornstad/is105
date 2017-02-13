e79e23b86f319cc098e2738ae83ddffdf87a1e42
snorre <snorre_btunge@hotmail.com>
2017-02-13 14:22:26 +0100

    uint64

---
+++ ICA02/oppg3.uint64.go
@@ -0,0 +1,34 @@
+package main
+// Når denne pakken heter noe annet enn main, skjer det ingenting når
+// "go build oppg3.int64.go" skrives i kommandolinjen. Altså det lages ikke en .exe fil.
+
+import "os"
+import "fmt"
+import "strconv"
+import "gitClone/is105/ICA02/sum"
+
+func main() {
+
+arg1 := os.Args[1]
+arg2 := os.Args[2]
+
+
+f1, err := strconv.ParseUint(arg1, 10, 64)
+if err == nil {
+  fmt.Println("  ",f1)
+}else{
+  fmt.Println("error")
+}
+
+
+f2, err2 := strconv.ParseUint(arg2, 10, 64)
+if err2 == nil {
+  fmt.Println("+ ",f2)
+}else{
+  fmt.Println("error")
+}
+
+resultat :=  sum.SumUint64(f1, f2)
+fmt.Println("= ", resultat)
+
+}
