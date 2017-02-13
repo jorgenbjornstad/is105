aac0a61ca9f18f39f933829218250a67fb11a352
vebnor <vebjon16@uia.no>
2017-02-13 20:59:40 +0100

    lagt til testfunksjoner (som feiler) for alle de forskjellige typene

--- ICA02/sum/sum_test.go
+++ ICA02/sum/sum_test.go
@@ -10,9 +10,78 @@ var sum_tests_int8 = []struct {
 }{
 	{1, 2, 3},
 	{4, 5, 9},
-	{120, 1, 119},
+	{118, 1, 119},
 }
+
+var sum_tests_int32 = []struct {
+	n1       int32
+	n2       int32
+	expected int32
+}{
+	{1, 2, 3},
+	{4, 5, 9},
+	{2147483646, 2, 2147483647},
 }
+
+var sum_tests_int64 = []struct {
+	n1       int64
+	n2       int64
+	expected int64
+}{
+	{1, 2, 3},
+	{4, 5, 9},
+	{118, 1, 180},
+}
+
+var sum_tests_float64 = []struct {
+	n1       float64
+	n2       float64
+	expected float64
+}{
+	{1, 2, 3},
+	{4, 5, 9},
+	{118, 1, 190},
+}
+
+var sum_tests_uint32 = []struct {
+	n1       uint32
+	n2       uint32
+	expected uint32
+}{
+	{1, 2, 3},
+	{4, 5, 9},
+	{10, 1, 9},
+}
+
+func TestSumInt32(t *testing.T) {
+	for _, v := range sum_tests_int32 {
+		if val := SumInt32(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+		}
+	}
+}
+func TestSumInt64(t *testing.T) {
+	for _, v := range sum_tests_int64 {
+		if val := SumInt64(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+		}
+	}
+}
+func TestSumFloat64(t *testing.T) {
+	for _, v := range sum_tests_float64 {
+		if val := SumFloat64(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+		}
+	}
+}
+func TestSumUint32(t *testing.T) {
+	for _, v := range sum_tests_uint32 {
+		if val := SumUint32(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+		}
+	}
+}
+
 
 func TestSumInt8(t *testing.T) {
 	for _, v := range sum_tests_int8 {
