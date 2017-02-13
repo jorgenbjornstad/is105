ce16580ec53142fd30b8835e77bfbd118069ea18
snorre <snorre_btunge@hotmail.com>
2017-02-13 13:37:04 +0100

    ny versjon av oppg3

--- ICA02/oppg3.go
+++ ICA02/oppg3.go
@@ -9,25 +9,23 @@ func main() {
 arg1 := os.Args[1]
 arg2 := os.Args[2]
 
-var f1 float64
-var err error
-
-f1, err = strconv.ParseFloat(arg1, 64)
+f1, err := strconv.ParseFloat(arg1, 64)
 if err == nil {
   fmt.Println("  ",f1)
 }else{
   fmt.Println("error")
 }
 
-var f2 float64
-var err2 error
-
-f2, err2 = strconv.ParseFloat(arg2, 64)
+f2, err2 := strconv.ParseFloat(arg2, 64)
 if err2 == nil {
   fmt.Println("+ ",f2)
 }else{
   fmt.Println("error")
 }
-sum := f1 + f2
+
+
+sum = f1 + f2
 fmt.Println("= ",sum)
+
+
 }
