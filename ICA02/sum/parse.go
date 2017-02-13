5d4700ddac6cd2aaaca1d31be8f793a72f1624e4
vebnor <vebjon16@uia.no>
2017-02-13 19:03:44 +0100

    funksjoner for å konvertere string til talltyper

---
+++ ICA02/sum/parse.go
@@ -0,0 +1,24 @@
+package sum
+
+import "strconv"
+import "fmt"
+
+func ConvertInt(input1 string) int64{
+  value, err := strconv.ParseInt(input1, 10, 64)
+  if err == nil {
+    fmt.Println(value)
+  }else{
+    fmt.Println("error")
+  }
+  return value
+  }
+
+func ConvertUint(input2 string) uint64{
+    value, err := strconv.ParseUint(input2, 10, 64)
+    if err == nil {
+      fmt.Println(value)
+    }else{
+      fmt.Println("error")
+    }
+    return value
+    }
