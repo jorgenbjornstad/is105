84ed95c3ea0dbf87e6cbc0dda5f3594465a50c5d
Jørgen Kongsberg <Jorgen_kongsberg@jrgens-mbp-3.uia.no>
2017-02-22 16:30:41 +0100

    definerte symoblene i const Ascii

--- ICA03/iso/iso.go
+++ ICA03/iso/iso.go
@@ -2,7 +2,10 @@ package iso
 
 func IterateOverASCIIStringLiteral(sl string) {
 	// Kode for Oppgave 2a
-}
 
+const Ascii = `€ ‚ƒ„…†‡ˆ‰Š‹Œ Ž  ‘’“”•–—˜™š›œ žŸ ¡¢`+
+`£¤¥¦§¨©ª«¬¬®¯°±²³´µ¶•¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔ`+
+`ÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ`
+}
 // Kode for Oppgave 2b
 func GreetingExtendedASCII() {}
