12ba8ac732148013874a03d7f19f667a9c5a8fe0
KimMoe <by-crom@hotmail.com>
2017-03-06 13:11:08 +0100

    fiksa på 4a

--- ICA03/unicode/unicode.go
+++ ICA03/unicode/unicode.go
@@ -1,9 +1,7 @@
 package unicode
 
-import "fmt"
-
 // Kode for Oppgave 4a
 func Translate(expression string, language string) string {
-	return ""
-	fmt.Printf("\x22\x6E\x6F\x72\x20\x6F\x67\x20\x73\xF8\x72")
+	return
+	//("\x22\x6E\x6F\x72\x20\x6F\x67\x20\x73\xF8\x72")
 }
