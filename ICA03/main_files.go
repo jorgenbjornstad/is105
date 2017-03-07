9168341791f88f66632ac7de84135dcfcfbad231
KimMoe <by-crom@hotmail.com>
2017-03-07 12:31:22 +0100

    gjorde endringer i main_fileutils

---
+++ ICA03/main_files.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"./fileutils"
+)
+
+func main() {
+	i := fileutils.FileToByteslice("C:/work/src/is105/ICA03/files/lang01.wl")
+	e := fileutils.FileToByteslice("C:/work/src/is105/ICA03/files/lang02.wl")
+	f := fileutils.FileToByteslice("C:/work/src/is105/ICA03/files/lang03.wl")
+	//fileTobyteslice.FileToByteslice(lang01.go, string) []
+	fmt.Printf("%b\n", i[:])
+	fmt.Printf("%X\n", e[:])
+	fmt.Printf("%s\n", f[:])
+}
