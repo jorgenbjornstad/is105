56899c1e5eb6aef5310681d966f0ff8a16662de2
vebnor <vebjon16@uia.no>
2017-03-09 11:19:11 +0100

    ICA04

---
+++ ICA04/main_byteslices.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+"is105/ICA03/fileutils"
+	"fmt"
+
+
+)
+func main() {
+	byteslice1 := fileutils.FileToByteslice("files/text1.txt")
+	byteslice2 := fileutils.FileToByteslice("files/text2.txt")
+	// fmt.Println(byteslice1)
+	// fmt.Printf("%s", byteslice1)
+	// fm.Printf("%q", byteslice1
+
+	fmt.Printf("%s", byteslice1)
+	fmt.Printf("%s", byteslice2)
+
+}
