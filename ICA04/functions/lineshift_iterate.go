57bbc5864dd2687907bad4a5cc439077432fda20
Vebjørn Nordby <vebjon16@uia.no>
2017-03-16 12:22:45 +0100

    fikset så den fungerer

--- ICA04/functions/lineshift_iterate.go
+++ ICA04/functions/lineshift_iterate.go
@@ -20,18 +20,12 @@ func Lineshift_iterate() {
 		log.Fatal(err)
 	}
 
-	if var1 := bytes.Contains([]byte(data)), []byte("\x0d\x0a"); var1 == true {
+	if var1 := bytes.Contains([]byte(data), []byte("\x0d\x0a")); var1 == true {
 		fmt.Println("This file uses CRLF.")
-	}else if var2 := bytes.Contains([]byte(data)), []byte("\x0a"); var2 == true {
+		}else if var2 := bytes.Contains([]byte(data), []byte("\x0a")); var2 == true {
 		fmt.Println("This file uses LF.")
-	}else if var3 := bytes.Contains([]byte(data)), []byte("\x0d"); var3 == true {
+	}else if var3 := bytes.Contains([]byte(data), []byte("\x0d")); var3 == true {
 			fmt.Println("This file uses CR.")
 			}
-
-
-
-	fmt.Printf("Data as hex: %x\n", var1)
-
-
 	fmt.Println("Number of bytes read:", len(data))
 }
