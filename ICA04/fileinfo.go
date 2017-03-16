69e7a0502fe82caf6ad343541a3e9e9c85d7f62e
vebnor <vebjon16@uia.no>
2017-03-16 16:57:20 +0100

    tror dette fungerer

--- ICA04/fileinfo.go
+++ ICA04/fileinfo.go
@@ -12,21 +12,26 @@ func main() {
 	fileInfo, err := os.Stat(arg)
 	if err != nil {
 		log.Fatal(err)
-		}
-		i := fileInfo.Size()
-
-		varB := float64(i)
-		varMB := varB / 1024
-		varGB := varMB / 1024
-		varTB := varGB / 1024
-		fmt.Println("Information about a file", arg, ":")
-		fmt.Println("Size:", varB, "in bytes.", varMB, "in megabytes.", varGB, " in gigabytes.", varTB, "in terrabytes.")
-		fmt.Println("Is Directory: ", fileInfo.IsDir())
-		fmt.Println("Is a regular file: ", fileInfo.Mode().IsRegular())
-	        fmt.Println("Is append only: ", fileInfo.Mode()&os.ModeAppend)
-		fmt.Println("Permissions:", fileInfo.Mode())
-		fmt.Println("Last modified:", fileInfo.ModTime())
-		fmt.Printf("System interface type: %T\n", fileInfo.Sys())
-		fmt.Printf("System info: %+v\n\n", fileInfo.Sys())
 	}
+
+	i := fileInfo.Size()
+
+	varB := float64(i)
+	varKiB := varB / 1024
+	varMiB := varKiB / 1024
+	varGiB := varMiB / 1024
+
+	fmt.Println("Information about a file", arg, ":")
+	fmt.Println("Size:", varB, "in bytes.", varKiB, "in kibibytes.", varMiB,
+		" in mibibytes.", varGiB, "in gibibytes.")
+	fmt.Println("Is Directory: ", fileInfo.Mode().IsDir())
+	fmt.Println("Is a regular file: ", fileInfo.Mode().IsRegular())
+	fmt.Println("Has UNIX permissions in bits: ", fileInfo.Mode())
+	fmt.Println("Is append only: ", fileInfo.Mode()&os.ModeAppend != 0)
+	fmt.Println("Is a device file:", fileInfo.Mode()&os.ModeDevice != 0)
+	fmt.Println("Is a UNIX character device: ", (fileInfo.Mode()&os.ModeDevice != 0)&&(fileInfo.Mode()&os.ModeCharDevice != 0))
+	fmt.Println("Is a UNIX block device: ", (fileInfo.Mode()&os.ModeDevice != 0)&&(fileInfo.Mode()&os.ModeCharDevice == 0))
+	fmt.Println("Is a symbolic link: ", fileInfo.Mode()&os.ModeSymlink != 0)
+
+}
 
