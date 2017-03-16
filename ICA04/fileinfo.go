849c55a10e6333208dc932b10bbaaaf7a8f2bfcf
Vebjørn Nordby <vebjon16@uia.no>
2017-03-16 15:05:40 +0100

    arbeidet videre på fileinfo.go og fikset liten importfeil på main_sum.go

--- ICA04/fileinfo.go
+++ ICA04/fileinfo.go
@@ -4,26 +4,29 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"./files"
-	"io/ioutil"
-)
-
-var (
-	fileInfo os.FileInfo
-	err      error
 )
 
 func main() {
-        arg := os.Args[1]
-	fileInfo, err = os.Stat(arg)
+
+	arg := os.Args[1]
+	fileInfo, err := os.Stat(arg)
 	if err != nil {
 		log.Fatal(err)
+		}
+		i := fileInfo.Size()
+
+		varB := float64(i)
+		varMB := varB / 1024
+		varGB := varMB / 1024
+		varTB := varGB / 1024
+		fmt.Println("Information about a file", arg, ":")
+		fmt.Println("Size:", varB, "in bytes.", varMB, "in megabytes.", varGB, " in gigabytes.", varTB, "in terrabytes.")
+		fmt.Println("Is Directory: ", fileInfo.IsDir())
+		fmt.Println("Is a regular file: ", fileInfo.Mode().IsRegular())
+	        fmt.Println("Is append only: ", fileInfo.Mode()&os.ModeAppend)
+		fmt.Println("Permissions:", fileInfo.Mode())
+		fmt.Println("Last modified:", fileInfo.ModTime())
+		fmt.Printf("System interface type: %T\n", fileInfo.Sys())
+		fmt.Printf("System info: %+v\n\n", fileInfo.Sys())
 	}
-	fmt.Println("File name:", fileInfo.Name())
-	fmt.Println("Size in bytes:", fileInfo.Size())
-	fmt.Println("Permissions:", fileInfo.Mode())
-	fmt.Println("Last modified:", fileInfo.ModTime())
-	fmt.Println("Is Directory: ", fileInfo.IsDir())
-	fmt.Printf("System interface type: %T\n", fileInfo.Sys())
-	fmt.Printf("System info: %+v\n\n", fileInfo.Sys())
-}
+
