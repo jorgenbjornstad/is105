ca3faae4be480f75f9504eae7c8efee5e31d3dee
Vebjørn Nordby <vebjon16@uia.no>
2017-04-04 14:16:40 +0200

    bitcoin html

---
+++ ICA05/functions/functions.go
@@ -0,0 +1,16 @@
+package functions
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func GetJSON(url string, target interface{}) error {
+	r, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
