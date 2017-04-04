712ae285521d7d6938d511121ae885cf4c4e553b
jorgenbjornstad <jorgenfbjornstad@gmail.com>
2017-04-04 18:07:27 +0200

    Lagt inn bitcoin currency, server.go kjøres gjennom port 8001

--- ICA05/functions/functions.go
+++ ICA05/functions/functions.go
@@ -5,6 +5,16 @@ import (
 	"net/http"
 )
 
+type btc struct {
+	Two4HAvg float64 `json:"avg"`
+	Ask float64 `json:"ask"`
+	Bid float64 `json:"bid"`
+	Last float64 `json:"last"`
+	Timestamp string `json:"timestamp"`
+	VolumeBtc float64 `json:"volume_btc"`
+	VolumePercent float64 `json:"volume_percent"`
+}
+
 func GetJSON(url string, target interface{}) error {
 	r, err := http.Get(url)
 	if err != nil {
@@ -13,4 +23,39 @@ func GetJSON(url string, target interface{}) error {
 	defer r.Body.Close()
 
 	return json.NewDecoder(r.Body).Decode(target)
+}
+
+func GetBTCUSD() *btc {
+	url := "https://api.bitcoinaverage.com/ticker/global/USD/"
+	bitUSD := new(btc)
+	GetJSON(url, bitUSD)
+	return bitUSD
+}
+
+func GetBTCEUR() *btc {
+	url := "https://api.bitcoinaverage.com/ticker/global/EUR/"
+	bitEUR := new(btc)
+	GetJSON(url, bitEUR)
+	return bitEUR
+}
+
+func GetBTCNOK() *btc {
+	url := "https://api.bitcoinaverage.com/ticker/global/NOK/"
+	bitNOK := new(btc)
+	GetJSON(url, bitNOK)
+	return bitNOK
+}
+
+func GetBTCGBP() *btc {
+	url := "https://api.bitcoinaverage.com/ticker/global/GBP/"
+	bitGBP := new(btc)
+	GetJSON(url, bitGBP)
+	return bitGBP
+}
+
+func GetBTCDKK() *btc {
+	url := "https://api.bitcoinaverage.com/ticker/global/DKK/"
+	bitDKK := new(btc)
+	GetJSON(url, bitDKK)
+	return bitDKK
 }
--- ICA05/server.go
+++ ICA05/server.go
@@ -1,71 +1,26 @@
 package main
 import (
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/render"
-
-	"time"
-	"strconv"
 	"net/http"
-	"log"
-	"io/ioutil"
-	"strings"
+	"path"
+	"./functions"
+	"html/template"
 )
 
-type SiteData struct {
-	Name string
-	Description string
-	Count string
-	ServerIP string
-	MapData string
-}
+func BTCUSD(w http.ResponseWriter, r *http.Request) {
+	btc := functions.GetBTCUSD()
+	fp := path.Join("templates", "index.tmpl")
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-var count int = 0
+	if err := tmpl.Execute(w, btc); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 func main() {
-
-	m := martini.Classic()
-
-	m.Use( render.Renderer(render.Options{
-		IndentJSON: true, // so we can read it..
-	}))
-
-	m.Get("/", func(r render.Render, x *http.Request) {
-		place := string(x.FormValue("place"))
-		place = strings.Replace(place, " ", "+", -1)
-
-		if len(place) <= 0 {place = "UIA+Kristiansand"}
-		r.HTML(200, "index", SiteData{"De grønne slakterne", "Veldig kjekke studenter", strconv.Itoa(count), getServerIP(), place})
-	})
-
-
-
-
-	m.Get("/getCount", func() string {
-		print(count)
-		return strconv.Itoa(count)
-	})
-
-
-	go countNumber()
-	m.RunOnAddr(":8001")
-	m.Run()
-
-
-}
-
-func getServerIP() string{
-	readApi, err := http.Get("https://api.ipify.org")
-	if err != nil {log.Fatal(err)}
-	bytes, err := ioutil.ReadAll(readApi.Body)
-	if err != nil {log.Fatal(err)}
-	return string(bytes)
-}
-
-func countNumber()  {
-	for true{
-		count += 1
-		time.Sleep(1 * time.Second)
-	}
-		
-
+	http.HandleFunc("/", BTCUSD)
+	http.ListenAndServe(":8001", nil)
 }
