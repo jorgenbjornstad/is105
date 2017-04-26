26ff717906fc8b89ba761047ef441ea6828831da
vebnor <vebjon16@uia.no>
2017-04-26 10:51:40 +0200

    la til snorre_engelsk.wav i wit-koden

--- ICA06/speech.go
+++ ICA06/speech.go
@@ -7,5 +7,6 @@ import ("github.com/nicolaifsf/go-speak"
 func main() {
 	speech.SetWitKey("JPTUX4UZQXYNBIZCGRRCB6GKPNRVJFUN")
 	fmt.Println(speech.SendWitVoice("wav/vebjorn_engelsk.wav"))
+	fmt.Println(speech.SendWitVoice("wav/snorre_engelsk.wav"))
 
 }
