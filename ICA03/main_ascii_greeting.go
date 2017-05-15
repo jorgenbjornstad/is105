6fce84c6af974926cf5d950306ea0597add8c925
juditheikaas <jijens13@student.uia.no>
2017-05-15 16:12:55 +0200

    fikset i koden på oppg1

--- ICA03/main_ascii_greeting.go
+++ ICA03/main_ascii_greeting.go
@@ -4,6 +4,9 @@ import "./ascii"
 import "fmt"
 
 func main() {
-print := ascii.GreetingASCII()
-fmt.Println(print)
+print1, print2, print3 := ascii.GreetingASCII()
+fmt.Printf("Den binære representasjonen av \"hello\": %b \n", print1, )
+fmt.Printf("Den hexadesimale representasjonen av \"hello\": %x \n", print2)
+fmt.Printf("%s \n", print3)
+
 }
