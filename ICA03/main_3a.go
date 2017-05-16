3e33a558a8333b24bbbbcaeafd6fef1b36dea581
snorre <snorre_btunge@hotmail.com>
2017-05-16 15:05:47 +0200

    fikset utskrift

--- ICA03/main_3a.go
+++ ICA03/main_3a.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 
-	fmt.Printf("Print med format %_s %s \n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
-	fmt.Printf("Print med format %_q %q \n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
-	fmt.Printf("Print med format %_+q %+q \n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
-	fmt.Printf("Print med format %_c %c \n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
+	fmt.Printf("Print med format %%s %s \n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
+	fmt.Printf("Print med format %%q %q \n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
+	fmt.Printf("Print med format %%+q %+q \n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
+	fmt.Printf("Print med format %%c %c \n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
 
 	fmt.Printf("%s \n", "\xc2\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
 }
