d1a0b69a3dca8624011e517281283a5addaf72ee
snorre <snorre_btunge@hotmail.com>
2017-05-16 15:25:42 +0200

    la til mer tekst

--- ICA03/main_3a.go
+++ ICA03/main_3a.go
@@ -9,5 +9,5 @@ func main() {
 	fmt.Printf("Print med format %%+q %+q \n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
 	fmt.Printf("Print med format %%c %c \n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
 
-	fmt.Printf("%s \n", "\xc2\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
+	fmt.Printf("La til en ekstra karakter (xc2) for å få 1/2: %s \n", "\xc2\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
 }
