e3a197fa4f8a7df268532eca004b4248c4357671
jorgenbjornstad <jorgenfbjornstad@gmail.com>
2017-05-16 13:24:50 +0200

    Dokumentert

--- ICA03/main_3b.go
+++ ICA03/main_3b.go
@@ -7,11 +7,13 @@ import (
 )
 
 func main () {
-  i := fileutils.FileToByteslice("files/lang01.wl")
-  e := fileutils.FileToByteslice("files/lang02.wl")
-  f := fileutils.FileToByteslice("files/lang03.wl")
-  //fileTobyteslice.FileToByteslice(lang01.go, string) []
-  fmt.Printf("% X \n", i[:])
-  fmt.Printf("% X \n", e[:])
-  fmt.Printf("% X \n", f[:])
+    // Constructs a byteslice from file.
+    i := fileutils.FileToByteslice("files/lang01.wl")
+    e := fileutils.FileToByteslice("files/lang02.wl")
+    f := fileutils.FileToByteslice("files/lang03.wl")
+
+    // Prints each constructed slice with %X.
+    fmt.Printf("% X \n", i[:])
+    fmt.Printf("% X \n", e[:])
+    fmt.Printf("% X \n", f[:])
 }
