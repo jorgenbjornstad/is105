c23a56cfd977450c94d7cc3fed92d797094db9d2
snorre <snorre_btunge@hotmail.com>
2017-05-19 20:13:35 +0200

    fikset

--- ICA04/4a.go
+++ ICA04/4a.go
@@ -3,16 +3,7 @@ package main
 import "./huffman"
 import "fmt"
 
-/*
-a := 000
-b := 001
-c := 111
-d := 10
-e := 110
-f := 01
-*/
-
 func main(){
-  x, _ := huffman.Decode("1110000011111011001")
-  fmt.Println(x)
+  print, _ := huffman.DecodeHuffmanString("1110000011111011001")
+  fmt.Println(print)
 }
