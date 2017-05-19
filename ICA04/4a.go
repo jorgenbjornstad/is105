ceebc0e35b610980184d778467bfc585905b0fad
snorre <snorre_btunge@hotmail.com>
2017-05-19 15:01:33 +0200

    test denne

---
+++ ICA04/4a.go
@@ -0,0 +1,17 @@
+package main
+
+import "./huffman"
+
+/*
+a := 000
+b := 100
+c := 111
+d := 01
+e := 011
+f := 10
+*/
+
+func main(){
+  huffman.Decode("0")
+}
+// "00010011101011101001011111100000"
